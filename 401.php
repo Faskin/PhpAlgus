79a790a1be618ea84c6b929c7c91cd73f9d17b90
Aleksei <aleksei@MacBook-Pro-Aleksei.local>
2021-11-09 10:02:28 +0200

    Initial commit

---
+++ 401.php
@@ -0,0 +1,32 @@
+<?php
+//andmed massiivis
+$data=[
+    "Nimi"=>"Aleksei Staiger",
+    "aadress"=>"Ringi 13",
+    "pilt"=> "guy.png",
+    "koduleht"=>"https://staiger19.thkit.ee"
+];
+?>
+<br>
+<a href="../hommik.php">tagasi... Ulesanne</a>
+<h1>Ulesanne 401</h1>
+
+<p> <b>
+        <?=$data["Nimi"]?>
+    </b>
+</p>
+<p><i>
+
+    <?=$data["aadress"]?>
+    </i>
+</p>
+<img src="<?=$data["pilt"]?>" " alt="pildike">
+<a href="<?=$data["koduleht"]?>" target="_blank">Aleksei Staiger koduleht</a>
+
+<?php
+$array=array("Aleksei Staiger", "Ringi 13", "guy.php", "staiger19.thkit.ee");
+echo "<b>Nimi:".$array[0]."</b><br>";
+echo "<i>Aadress:".$array[1]."</i><br>";
+echo "<img src='$array[2]' alt='pildike'>";
+echo "<br><a href='hhtps://$array[3]'>Koduleht</a>";
+?>
---
+++ hommik.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ee">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="https://kit.fontawesome.com/1f326e6c46.js" crossorigin="anonymous"></script>
+    <title>PHP algus</title>
+</head>
+
+
+<body>
+<?php
+echo "Tere hommikust!";
+$tekst="Tana on esimene PHP tund";
+echo $tekst;
+echo "<br>";
+echo "<h2>Matemaatika tehed. Moistatus</h2>";
+$arv1=8;
+$arv2=11;
+echo "Kui liidame kokku, vastus on".($arv1+$arv2);
+?>
+</body>
+</html>
